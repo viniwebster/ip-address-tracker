310704f2269e3f401928714335f7ef0b67b3b865
vinicius webster <viniciuswebster99@gmail.com>
2023-05-01 13:26:57 -0300

    tratamento de erro

--- scripts/conectaApi.js
+++ scripts/conectaApi.js
@@ -7,26 +7,42 @@ userApiInfos()
 const search = document.getElementById('search');
 const button = document.getElementById('btn');
 const mapa = document.getElementById('map-bx');
+const erro = document.getElementById('erro');
 
 
 button.addEventListener('click', ()=>{
-    mapa.innerHTML = ""
     conectaApi(search.value);
     search.value = "";
 })
 
+search.addEventListener('keydown', (e)=>{
+    if(e.key === 'Enter'){
+        conectaApi(search.value);
+        search.value = "";
+    }    
+})
+
 
 async function conectaApi(search){
+    erro.textContent = "";   
+    try{
+        let api = await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_qPFAzE6MC7Kp9mGCLFTasDBo0zvm2&domain=${search}`);
+        let apiConvertida = await api.json();
+
+        if (!api.ok) {
+            throw new Error('Insert a valid ip or domain')
+        }
+        
+        mapa.innerHTML = `<div id="map" class="container__image-map"></div>`
+        addInfos(apiConvertida.ip, apiConvertida.location, apiConvertida.location.timezone, apiConvertida.isp);
+        addPopUp(apiConvertida.location.lat, apiConvertida.location.lng);
+
+        console.log(apiConvertida);
+    } catch (e) {
+        erro.textContent = 'Insert a valid ip or domain';
+        console.log(e);
+    }
     
-    let api = await fetch(`https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_qPFAzE6MC7Kp9mGCLFTasDBo0zvm2&domain=${search}`);
-    let apiConvertida = await api.json();
-
-    mapa.innerHTML = `<div id="map" class="container__image-map"></div>`
-    addInfos(apiConvertida.ip, apiConvertida.location, apiConvertida.location.timezone, apiConvertida.isp);
-    addPopUp(apiConvertida.location.lat, apiConvertida.location.lng);
-
-    console.log(apiConvertida);
-
 }
 
 
